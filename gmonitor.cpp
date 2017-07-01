8690caf70ae58f428486374b470e863743f9e5e2
littledj <littledj@163.com>
2017-07-01 18:04:51 +0800

    aa

--- gmonitor.cpp
+++ gmonitor.cpp
@@ -1,9 +1,24 @@
+#include <mutex>
 #include "gmonitor.h"
 #include "windows.h"
+
+mutex g_monitorMutex;      
 
 GMonitor::GMonitor(MONITOR_THREAD_PARA* para):GThread(para)
 {
     m_para = para;
+
+    m_hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    m_program = TEXT("<no name>");
+    m_version = TEXT("v1.0.0");
+    m_screenWidth = 80;
+    m_screenHeight = 6;
+    m_idSize = 20;
+    m_valueSize = m_screenWidth - 7 - m_idSize;
+    m_valueHalfSize = (m_screenWidth - 7) / 2 - m_idSize - 3;
+    m_sizeChanged = true;
+
+    // 处
 }
 
 
@@ -24,11 +39,223 @@ void GMonitor::ThreadMain()
 
 }
 
-void GMonitor::UpdateContent(tstring name, int value)
+void GMonitor::SetProgramName(tstring name, tstring version)
 {
+    if ((m_welcome.size() + name.size() + version.size() + 6) > m_screenWidth)
+    {
+        // show error info
+        return;
+    }
+
+    m_program = name;
+    m_version = version;
 }
 
-void GMonitor::gotoxy(short x, short y)
+void GMonitor::SetScreenWidth(uint32_t width)
+{
+    uint32_t w = width < MIN_MONITOR_WIDTH ? MIN_MONITOR_WIDTH : width;
+    w = w > MAX_MONITOR_WIDTH ? MAX_MONITOR_WIDTH : w;
+
+    if ((m_welcome.size() + m_program.size() + m_version.size() + 6) > w)
+    {
+        // show error info
+        return;
+    }
+
+    g_monitorMutex.lock();
+    m_screenWidth = w;
+    m_valueSize = m_screenWidth - 7 - m_idSize;
+    m_valueHalfSize = (m_screenWidth - 7) / 2 - m_idSize - 3;
+    m_sizeChanged = true;
+    g_monitorMutex.unlock();
+}
+
+void GMonitor::RegisterItem(tstring id, MONITOR_COLOR color, uint32_t refresh)
+{
+    if (id.size() > (m_screenWidth / 2))
+    {
+        return;
+    }
+
+    g_monitorMutex.lock();
+    // 项名称长度改变
+    if (id.size() > m_idSize)
+    {
+        m_idSize = id.size();
+        m_valueSize = m_screenWidth - 7 - m_idSize;
+        m_valueHalfSize = (m_screenWidth - 7) / 2 - m_idSize - 3;
+        m_sizeChanged = true;
+    }
+
+    // 注册item 
+    m_items.insert_or_assign(id, 
+        MONITOR_ITEM({id, TEXT(""), true, ITEM_SIZE::LINE_HALF, color, refresh}));    
+    m_sizeChanged = true;
+    if (m_items.size() % 2 != 0)
+    {
+        m_screenHeight += 2;
+    }
+    g_monitorMutex.unlock();
+}
+
+void GMonitor::SetItemValue(tstring id, const tstring format, ...)
+{
+    auto item = m_items.find(id);
+    if (item == m_items.end())
+    {
+        // 状态栏
+        return;
+    }
+    if (id.size() > (m_screenWidth / 2))
+    {
+        return;
+    }
+
+    // format value
+    va_list ap;
+    va_start(ap, format);
+    tstring value;
+    if (format.find('%') != tstring::npos)
+    {
+        tchar* value_tmp = new tchar[format.length() + 1024 + 2];
+        vsntprintf(value_tmp, 1024 + 2, format.c_str(), ap);
+        value.assign(value_tmp);
+        delete[] value_tmp;
+    }
+    else
+    {
+        value = format;
+    }
+    va_end(ap);        
+    
+    // calc value size
+    ITEM_SIZE vsize = ITEM_SIZE::LINE_HALF;
+    if (value.size() > (m_valueSize * 3))
+    {
+        // cut
+        value.erase(value.begin() + m_valueSize * 3 - 3, value.end());
+        value.append(TEXT("..."));
+        vsize = ITEM_SIZE::LINE_3;
+    }
+    else if (value.size() > (m_valueSize * 2))
+        vsize = ITEM_SIZE::LINE_3;
+    else if (value.size() > m_valueSize)
+        vsize = item->second.size > ITEM_SIZE::LINE_2 ? item->second.size : ITEM_SIZE::LINE_2;
+    else if (value.size() > m_valueHalfSize)
+        vsize = item->second.size > ITEM_SIZE::LINE_1 ? item->second.size : ITEM_SIZE::LINE_1;
+    else
+        vsize = item->second.size > ITEM_SIZE::LINE_HALF ? item->second.size : ITEM_SIZE::LINE_HALF;
+
+    g_monitorMutex.lock();
+    // 项值所占行数改变
+    if (vsize > item->second.size)
+    {
+        m_screenHeight += (int)vsize - (int)item->second.size + 1;
+        m_sizeChanged = true;
+    }    
+    item->second.changed = true;
+    item->second.size = vsize;
+    item->second.value = value;
+    g_monitorMutex.unlock();
+}
+
+void GMonitor::UpdateContent()
+{
+    // refresh every second
+    static time_t tt = time(NULL);
+    if (time(NULL) == tt)
+        return;
+
+    // need to refresh all
+    if (m_sizeChanged)
+    {
+        // rebuild UI table
+        RefreshTable();
+
+        // need refresh all items
+        for (auto item : m_items)
+        {
+            item.second.changed = true;
+        }
+    }
+
+    for (auto item : m_items)
+    {
+        if (item.second.changed)
+        {
+            RefreshData(item.second, );
+        }
+    }
+}
+
+void GMonitor::RefreshTable()
+{
+    // 清除所有
+    for (size_t y = 0; y < m_screenHeight; y++)
+    {
+        SetPrintCur(0, y);
+        for (size_t  x = 0; x < m_screenWidth; x++)
+        {            
+            tprintf(TEXT(" "));
+        }
+    }
+
+    // 边框颜色
+    SetConsoleTextAttribute(m_hStdOut, (WORD)MONITOR_COLOR::DARK_WHITE);
+
+    // 外上边框
+    size_t x = 0, y = 0;
+    SetPrintCur(0, 0);
+    for (size_t i = 0; i < m_screenWidth; i++)
+        tprintf(TEXT("="));
+    tprintf(TEXT("\n"));y++;
+
+    // 标题
+    tprintf(TEXT("|"));
+    size_t x = (m_screenWidth - (m_welcome.size() + m_program.size() + m_version.size() + 2)) / 2;
+    SetPrintCur(x, y);
+    SetConsoleTextAttribute(m_hStdOut, (WORD)MONITOR_COLOR::BRIGHT_CYAN);
+    tprintf((m_welcome + TEXT(" ")).c_str());
+    SetConsoleTextAttribute(m_hStdOut, (WORD)MONITOR_COLOR::BRIGHT_PURPLE);
+    tprintf((m_program + TEXT(" ")).c_str());
+    SetConsoleTextAttribute(m_hStdOut, (WORD)MONITOR_COLOR::BRIGHT_CYAN);
+    tprintf(m_version.c_str());
+    
+    // 标题下边框
+    tprintf(TEXT("|"));
+    for (size_t i = 1; i < m_screenWidth-1; i++)
+        tprintf(TEXT("="));
+    tprintf(TEXT("|"));
+
+    // 外左边框
+    SetPrintCur(0, m_screenHeight - 2);
+    
+    // 状态栏
+
+    // 外下边框
+    SetPrintCur(0, m_screenHeight - 2);
+    for (size_t i = 0; i < m_screenWidth; i++)
+        tprintf(TEXT("="));
+}
+
+void GMonitor::RefreshData(const MONITOR_ITEM &item, const COORD pos)
+{
+    DWORD count;
+    FillConsoleOutputCharacter(m_hStdOut, ' ', maxLen, pos, &count);
+    SetConsoleCursorPosition(m_hStdOut, pos);
+    if (data.size() <= maxLen)
+    {
+        SetConsoleTextAttribute(m_hStdOut, (WORD)color);
+        WriteConsole(m_hStdOut, data.c_str(), data.size(), &count, 0);
+    }
+    else
+    {
+        SetConsoleTextAttribute(m_hStdOut, (WORD)PRINT_COLOR::DARK_YELLOW);
+        WriteConsole(m_hStdOut, m_defaultValue.c_str(), m_defaultValue.size(), &count, 0);
+    }
+}
+
+void GMonitor::SetPrintCur(short x, short y)
 {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { x, y });
 }
--- gmonitor.h
+++ gmonitor.h
@@ -1,7 +1,40 @@
+//
+
 #pragma once
+#include <map>
 #include "tstream.h"
 #include "gthread.h"
-#include "glogger.h"
+
+#define MIN_MONITOR_WIDTH 40
+#define MIN_MONITOR_HEIGHT 8
+#define MAX_MONITOR_WIDTH 150
+#define MAX_MONITOR_HEIGHT 40
+
+enum class MONITOR_COLOR
+{
+    DARK_BLUE = 1,
+    DARK_GREEN = 2,
+    DARK_CYAN = 3,
+    DARK_RED = 4,
+    DARK_PURPLE = 5,
+    DARK_YELLOW = 6,
+    DARK_WHITE = 7,
+    BRIGHT_BLUE = 9,
+    BRIGHT_GREEN = 10,
+    BRIGHT_CYAN = 11,
+    BRIGHT_RED = 12,
+    BRIGHT_PURPLE = 13,
+    BRIGHT_YELLOW = 14,
+    BRIGHT_WHITE = 15,
+};
+
+enum class ITEM_SIZE
+{
+    LINE_HALF = 0,
+    LINE_1 = 1,
+    LINE_2 = 2,
+    LINE_3 = 3
+};
 
 using namespace gcommon;
 
@@ -13,6 +46,16 @@ typedef struct _monitor_para : _thread_para
     int test;
 }MONITOR_THREAD_PARA, *PMONITOR_THREAD_PARA;
 
+typedef struct _monitor_item
+{
+    tstring id;
+    tstring value;
+    bool changed;
+    ITEM_SIZE size;
+    MONITOR_COLOR color;
+    uint32_t refresh_interval; // ∫¡√Î
+}MONITOR_ITEM;
+
 #pragma pack(pop)
 
 
@@ -23,18 +66,38 @@ public:
     ~GMonitor();
 
 private:
+    const tstring m_welcome = TEXT("Welcome to");
+
+    HANDLE m_hStdOut;
+    tstring m_program;
+    tstring m_version;
+    uint32_t m_screenWidth;
+    uint32_t m_screenHeight;
+    uint32_t m_idSize; 
+    uint32_t m_valueHalfSize; // half line value max size
+    uint32_t m_valueSize; // one line value max size
+    bool m_sizeChanged;
     MONITOR_THREAD_PARA* m_para;
-    bool bupdate;
+    map<tstring, MONITOR_ITEM> m_items;
 
 public:
     void ThreadMain();
-    void SetTitle(tstring title);
+
+    // setup gmonitor
+    void SetProgramName(tstring name, tstring version = TEXT("v1.0.0"));
+    void SetScreenWidth(uint32_t width = 80);
     void SetContent(tstring group, tstring name);
 
-    void UpdateContent(tstring name, int value);
+    // handle items
+    void RegisterItem(tstring id, MONITOR_COLOR color = MONITOR_COLOR::DARK_WHITE, uint32_t refresh = 1000);
+
+    // update values
+    void SetItemValue(tstring id, const tstring format, ...);
+    void UpdateContent();
 
 private:
-
-    void gotoxy(short x, short y);
+    void RefreshTable();
+    void RefreshData(const MONITOR_ITEM &item, const COORD pos);
+    void SetPrintCur(short x, short y);
 };
 
